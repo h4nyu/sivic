01b6af972e5474dc75d8fe1015dd1d69414f80c3
xinyuan-yao <yao.ntno@gmail.com>
2021-01-17 13:52:45 +0000

    add entities

---
+++ app/packages/core/workspace.ts
@@ -0,0 +1,26 @@
+import { v4 as uuid } from 'uuid';
+
+export type Workspace = {
+  id: string
+  name: string
+  createdAt: Date
+}
+
+export type WorkspaceImage = {
+  workspaceId: string
+  imageId: string
+}
+
+export const Workspace = (args?: Workspace):Workspace => {
+  return args || {
+    id: uuid(),
+    name: "",
+    createdAt: new Date(),
+  }
+}
+export const Service = () => {
+  const uploadImages = async () => { }
+  return {
+    uploadImages
+  }
+}
