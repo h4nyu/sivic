24f474b13d3bc7c5622f71bd6630e59dc29e8a24
higuchi <ce51046.higuchi@gmail.com>
2021-04-28 22:46:46 +0900

    fix

--- app/packages/core/utils.spec.ts
+++ app/packages/core/utils.spec.ts
@@ -1,9 +1,9 @@
 import {
-  getBaseline, Point
+  rotatePoint, getBaseline, Point
 } from "@sivic/core/utils";
 
 describe("spec", () => {
-  test("create and filter", async () => {
+  test("getBaseline", async () => {
     const points = [
       {...Point(), x:0, y:0},
       {...Point(), x:2, y:2},
@@ -15,5 +15,13 @@ describe("spec", () => {
       {...Point(), x:0, y:0},
       {...Point(), x:5, y:5}
     ])
+  });
+  test("getBaseline", async () => {
+    const point = {...Point(), x:5, y:1}
+    const originPoint = {...Point(), x:1, y:1}
+    const res = rotatePoint(point, originPoint, 90)
+    expect(res).toEqual(
+      {...Point(), x:1, y:5},
+    )
   });
 });
