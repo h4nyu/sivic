90bb7c364b39a05021a1f1a6bb43e7224bb4ecc8
xinyuan.yao <yao.ntno@gmail.com>
2021-07-09 18:34:13 +0900

    fix type

--- app/packages/core/utils.spec.ts
+++ app/packages/core/utils.spec.ts
@@ -1,27 +1,27 @@
 import {
-  rotatePoint, getBaseline, Point
+  rotatePoint, getBaseline
 } from "@sivic/core/utils";
+import { Point } from "@sivic/core/point"
 
 describe("spec", () => {
   test("getBaseline", async () => {
     const points = [
-      {...Point(), x:0, y:0},
-      {...Point(), x:2, y:2},
-      {...Point(), x:5, y:5}
+      Point({ x:0, y:0}),
+      Point({ x:2, y:2}),
+      Point({ x:5, y:5})
     ]
     const baseline = getBaseline(points)
-    console.log(baseline)
     expect(baseline).toEqual([
-      {...Point(), x:0, y:0},
-      {...Point(), x:5, y:5}
+      Point({x:0, y:0}),
+      Point({x:5, y:5})
     ])
   });
   test("getBaseline", async () => {
-    const point = {...Point(), x:5, y:1}
-    const originPoint = {...Point(), x:1, y:1}
+    const point = Point({x:5, y:1})
+    const originPoint = Point({ x:1, y:1})
     const res = rotatePoint(point, originPoint, 90)
     expect(res).toEqual(
-      {...Point(), x:1, y:5},
+      Point({x:1, y:5}),
     )
   });
 });
