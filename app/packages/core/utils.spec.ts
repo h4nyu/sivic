8db758e1d5eeebaa1ef9364969bab954a3a51835
xinyuan.yao <yao.ntno@gmail.com>
2021-07-13 01:00:50 +0900

    fix merge

--- app/packages/core/utils.spec.ts
+++ app/packages/core/utils.spec.ts
@@ -6,36 +6,18 @@ import { Point } from "@sivic/core/point"
 describe("spec", () => {
   test("getBaseline", async () => {
     const points = [
-<<<<<<< HEAD
       Point({x:0, y:0}), 
       Point({x:2, y:2}), 
       Point({x:5, y:5}),
-||||||| 8826f25
-      {...Point(), x:0, y:0},
-      {...Point(), x:2, y:2},
-      {...Point(), x:5, y:5}
-=======
-      Point({ x:0, y:0}),
-      Point({ x:2, y:2}),
-      Point({ x:5, y:5})
->>>>>>> 3017da5db2033f62c5f4cd2e94d23baf74dde6ae
     ]
     const baseline = getBaseline(points)
     expect(baseline).toEqual([
-<<<<<<< HEAD
       Point({
         x:0, y:0
       }),
       Point({
         x:5, y:5
       })
-||||||| 8826f25
-      {...Point(), x:0, y:0},
-      {...Point(), x:5, y:5}
-=======
-      Point({x:0, y:0}),
-      Point({x:5, y:5})
->>>>>>> 3017da5db2033f62c5f4cd2e94d23baf74dde6ae
     ])
   });
   test("getBaseline", async () => {
